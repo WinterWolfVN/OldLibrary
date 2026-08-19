9b18dc4a909ad0d07f6f55162f6cd05fdc9725d8
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-08-19 22:34:12 +0700

    Add InMemoryDexClassLoader class implementation

---
+++ oldlib/dalvik/system/InMemoryDexClassLoader.java
@@ -0,0 +1,21 @@
+package oldlib.dalvik.system;
+
+import oldlib.dalvik.system.BaseDexClassLoader;
+import java.nio.ByteBuffer;
+
+public final class InMemoryDexClassLoader
+        extends BaseDexClassLoader {                  
+                
+    public InMemoryDexClassLoader(
+            ByteBuffer[] dexBuffers,
+            ClassLoader parent) {
+        super(dexBuffers, parent);
+    }
+
+    public InMemoryDexClassLoader(
+            ByteBuffer dexBuffer,
+            ClassLoader parent) {
+        this(new ByteBuffer[]{dexBuffer}, parent);
+    }
+}
+
